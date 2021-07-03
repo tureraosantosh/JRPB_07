60e3ce079405034e14dcd07e2c4a98f9326feff5
tureraosantosh <tureraosantosh@gmail.com>
2021-07-03 18:47:01 +0530

    updated session-05

---
+++ Part-01-Basic-Java/Session-05-Statement/src/statements/conditional/IfElseExample.java
@@ -0,0 +1,17 @@
+package statements.conditional;
+
+public class IfElseExample {
+
+	public static void main(String[] args) {
+		int mark;
+		mark = 5;
+
+		if (mark > 40) {
+			System.out.println("You Are Passed");
+		} else {
+			System.out.println("You are Eligible to Enjoy one more Year !!!!");
+		}
+
+	}
+
+}
