e8410e964149371421ae7808598cda723e3d42d0
tureraosantosh <tureraosantosh@gmail.com>
2021-07-05 19:58:39 +0530

    updates

--- Part-01-Basic-Java/Session-06-Statement-multiple/src/muitiple/SwitchCase.java
+++ Part-01-Basic-Java/Session-06-Statement-multiple/src/muitiple/SwitchCase.java
@@ -7,32 +7,31 @@ public class SwitchCase {
 	 * 
 	 * @param args
 	 */
-	
+
 	public static void main(String[] args) {
-		
+
 		/**
 		 * this object is used to get data from user/command line.
 		 */
-		Scanner  sc=new Scanner(System.in);
-		
+		Scanner sc = new Scanner(System.in);
+
 		int choice;
-		
+
 		System.out.println("1.Veg 2.Non-Veg");
-		
-		choice=1;
-		
-		switch(choice)
-		{
+
+		choice = 1;
+
+		switch (choice) {
 		case 1:
 			System.out.println("Your choice is Veg ");
 			break;
-			
+
 		case 2:
 			System.out.println("Your Choice is Non-Veg..");
 			break;
-		
+
 		}
-		
+
 	}
 
 }
